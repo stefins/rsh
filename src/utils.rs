a633cab5feb39ece30fe44f1bdd4df455ba2774b
Stefin <stefin@pm.me>
2022-02-10 09:48:27 +0530

    changing terminal into raw mode

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,15 @@
 use std::{
+    env,
+    env::current_dir,
     ffi::CString,
     sync::mpsc::{Receiver, Sender},
 };
 
-use libc::{chdir, kill, SIGINT};
+use std::mem::MaybeUninit;
+
+use libc::{
+    chdir, kill, tcgetattr, tcsetattr, termios, ECHO, ICANON, ISIG, SIGINT, STDIN_FILENO, TCSAFLUSH,
+};
 
 // This function setup a thread to handle ctrl+c INT's
 // The thread recieves a pid from a channel and send SIGINT to the pid
@@ -33,9 +39,30 @@ pub(crate) fn trim_newline(s: &mut String) {
 
 // wrapper around chdir syscall
 pub(crate) fn change_dir(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("OLDPWD", current_dir()?);
     unsafe {
         let path = CString::new(path)?;
         chdir(path.as_ptr());
         Ok(())
+    }
+}
+
+// Make the terminal to raw_mode
+pub(crate) fn enable_raw_mode() -> MaybeUninit<termios> {
+    let mut orig_termios: MaybeUninit<termios> = MaybeUninit::uninit();
+    unsafe {
+        let mut raw = orig_termios;
+        tcgetattr(STDIN_FILENO, raw.as_mut_ptr());
+        orig_termios = raw;
+        (*raw.as_mut_ptr()).c_lflag &= !(ECHO | ICANON | ISIG);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, raw.as_mut_ptr());
+    }
+    orig_termios
+}
+
+// Revert the terminal back to canonical mode
+pub(crate) fn disable_raw_mode(mut orig_termios: MaybeUninit<termios>) {
+    unsafe {
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios.as_mut_ptr());
     }
 }
